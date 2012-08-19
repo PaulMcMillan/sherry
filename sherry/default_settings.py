12ae76a973e58ba605bcb7cd5f39f07aaf2edaca
Paul McMillan <paul.mcmillan@nebula.com>
2012-08-18 21:50:25 -0700

    Add reimage route.

--- sherry/default_settings.py
+++ sherry/default_settings.py
@@ -1,2 +1,5 @@
+# Is this a good hack? A bad one? It's a bit weird...
+#from sherry.power import IPMIDriver as POWER_DRIVER
+from sherry.power import MockPowerDriver as POWER_DRIVER
 SERVER_NAME = 'localhost:24602'
 DEBUG = True
--- sherry/flask_app.py
+++ sherry/flask_app.py
@@ -6,31 +6,44 @@ app = Flask(__name__)
 app.config.from_object('sherry.default_settings')
 #app.config.from_envvar('SHERRY_SETTINGS')
 
-systems_to_install = {'mac_addr': {'release':123, 'kernel_opts':'foobar'}}
+reimage_queue = {'mac_addr': {'release':123, 'kernel_opts':'foobar'}}
 
 @app.route('/')
 def foo():
-    return "%s\n%s" % (app.config['SERVER_NAME'], systems_to_install)
+    return "%s\n%s" % (app.config['SERVER_NAME'], reimage_queue)
 
 @app.route('/pxe/chain.pxe', methods=['GET'])
 def chain_pxe():
     return render_template('chain.pxe')
 
 @app.route('/pxe/<mac_address>', methods=['GET'])
-def sort_system(mac_address):
-    if mac_address not in systems_to_install:
+def boot_or_reimage(mac_address):
+    # TODO (PaulM): regularize the mac address
+    if mac_address not in reimage_queue:
         return render_template('boot.pxe')
-    system_info = systems_to_install[mac_address]
+    # Remove the system from the queue
+    system_info = reimage_queue.pop(mac_address)
     return render_template('install.pxe', **system_info)
 
-# This allows both methods for convenience. Bookmark reimage ftw!
-@app.route('/install', methods=['GET', 'POST'])
-def install():
-    # ipmi_address
-    # mac_address
-    # release
-    # kernel_opts
-    pass
+# allows both HTTP methods for convenience. Bookmark reimage ftw!
+@app.route('/reimage', methods=['GET', 'POST'])
+def reimage():
+    """ Initiate a reimage of the target system.
+    Parameters:
+    - ipmi_address
+    - mac_address
+    - release
+    - kernel
+    """
+    req_vals = request.values.to_dict()
+    ipmi_address = req_vals.pop('ipmi_address')
+    mac_address = req_vals.pop('mac_address')
+    # store the rest of the parameters to render the template
+    reimage_queue[mac_address] = req_vals
+    # TODO (PaulM): pre-render the template(s) with these values here
+    powerdriver = app.config['POWER_DRIVER'](ipmi_address, 'user', 'pass')
+    powerdriver.reboot()
+    return "it might have worked..."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=24602)
