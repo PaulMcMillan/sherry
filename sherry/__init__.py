df5dd28f19a6e82164ecda9278cd73dfef78f048
Paul McMillan <paul.mcmillan@nebula.com>
2012-08-19 02:16:36 -0700

    Use strict template rendering.

--- sherry/__init__.py
+++ sherry/__init__.py
@@ -1,6 +1,10 @@
+import jinja2
 from flask import Flask
+from sherry import converters
 
 app = Flask(__name__)
 app.config.from_object('sherry.default_settings')
+app.url_map.converters['mac'] = converters.MacConverter
+app.jinja_env.undefined = jinja2.StrictUndefined
 
 import sherry.views
