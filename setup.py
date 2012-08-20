abc85284c99b861d5b311eb185b860200bf0c09e
Paul McMillan <paul.mcmillan@nebula.com>
2012-08-20 12:30:54 -0700

    Minor python setup changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='Sherry',
     version='0.1',
-    description='Sherry makes pxe easy',
+    description='A (nearly) stateless netboot enabler',
     url='https://github.com/PaulMcMillan/sherry',
     packages=['sherry'],
     include_package_data=True,
